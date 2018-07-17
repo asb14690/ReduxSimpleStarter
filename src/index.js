1abc472fcb7c1a1fd8687abb6c5fdeeffdc27954
Ankur <ankur.sharma@technocube.in>
2018-07-17 11:07:51 +0530

    Added chaanges

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react' // Import React modules in ES6
+import ReactDOM from 'react-dom'
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyAmIP8fwTlIvIEuMO_Jm11es4i3vgVCgVg'
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a component with JSX that produce HTML
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
+
+// Take genrated HTML and put on the page(DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'))
